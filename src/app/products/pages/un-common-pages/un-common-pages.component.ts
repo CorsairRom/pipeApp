296825f50ffc0b8c4a0a807853d01a612a047c88
CorsairRom <ric.romero@duocuc.cl>
2023-07-06 19:51:49 -0400

    pipes

--- src/app/products/pages/un-common-pages/un-common-pages.component.ts
+++ src/app/products/pages/un-common-pages/un-common-pages.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-un-common-pages',
@@ -19,4 +20,39 @@ export class UnCommonPagesComponent {
     this.name = 'Melissa';
     this.gender = 'female';
   }
+
+  //i18nPlural
+  public clients:string[] =['Maria', 'Pedro', 'Fernando', 'Armando', 'Eduardo', 'Melissa', 'Natalia'];
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  deleteClient():void {
+    this.clients.shift();
+  }
+
+  //KeyValue Pipe
+
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada'
+  }
+
+  //Async Pipe
+
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap: ',value))
+  );
+
+  public promiseValues: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      resolve('Tenemos data en la promesa.')
+    }, 3500);
+  })
+
+
 }
